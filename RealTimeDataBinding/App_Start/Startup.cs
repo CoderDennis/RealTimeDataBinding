36eb72309319e02dc86f070da794f1be674e0614
CoderDennis <dodava@gmail.com>
2015-01-05 12:38:02 -0600

    progress on adding stock ticker to Durandal app. It doesn't work yet...

---
+++ RealTimeDataBinding/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RealTimeDataBinding.App_Start.Startup), "Configuration")]
+
+namespace RealTimeDataBinding.App_Start
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+
+    }
+}
